15b3a76d0a4117b9f714d33ef84d32e4dde4385a
MD AFNAN QUAMAR <mdafnankhan786@gmail.com>
2026-09-07 10:30:39 +0530

    Change API base URL to production endpoint

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-resume-analyzer-8o02.onrender.com",
     withCredentials: true,
 })
 
