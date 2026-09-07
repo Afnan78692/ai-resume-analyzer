efa6466f082904b416076de4405b860513817094
MD AFNAN QUAMAR <mdafnankhan786@gmail.com>
2026-09-07 10:31:05 +0530

    Remove trailing slash from baseURL in auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "https://ai-resume-analyzer-8o02.onrender.com/",
+    baseURL: "https://ai-resume-analyzer-8o02.onrender.com",
     withCredentials: true
 })
 
